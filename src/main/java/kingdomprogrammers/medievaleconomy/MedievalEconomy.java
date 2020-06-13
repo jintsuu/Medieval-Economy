db53f54b4e629f0067555c9f614849e9858d90db
dmccoystephenson <dmccoystephenson@gmail.com>
2020-06-12 19:26:59 -0600

    initial commit

---
+++ src/main/java/kingdomprogrammers/medievaleconomy/MedievalEconomy.java
@@ -0,0 +1,20 @@
+package kingdomprogrammers.medievaleconomy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MedievalEconomy extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+
+
+    }
+}
